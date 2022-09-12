0528eda450602ffaca050cf032e72e3fe64ab217
Väinö Jämsä <105347805+Cefucr@users.noreply.github.com>
2022-09-12 12:53:23 +0300

    Update henk.php

--- henk.php
+++ henk.php
@@ -40,57 +40,69 @@
         $hei = 0;
         
         //katsoo jos input on 11 kirjainta
-        if(strlen($terve) == 11){        
-            $hei = $hei + 0;
-             //katsoo jos päivämäärä voi olla oikea
-            if($terve[0].$terve[1] > 31 || $terve[0].$terve[1] < 1){
-                $hei = $hei + 1;
-            }
-            else{
-                $hei = $hei + 0;
-            }
-            //katsoo jos kuukausi voi olla oikea
-            if($terve[2].$terve[3] > 12 || $terve[2].$terve[3] < 1){
-                $hei = $hei + 1;
-            }
-            else{
-                $hei = $hei + 0;
-            }
-            //katsoo jos vuosi voi olla oikea
-            if($terve[4].$terve[5] > 99 || $terve[4].$terve[5] < 0){
-                $hei = $hei + 1;
-            }
-            else{
-                $hei = $hei + 0;
-            }
-            //katsoo jos 7 inputin kirjai on A , - tai +
-            if (str_contains($terve[6], '-')||str_contains($terve[6], 'A')||str_contains($terve[6], '+')){
-                $hei = $hei + 0;
-            }
-            else{
-                $hei = $hei +1;
-                
-            }
-            //katsoo jos 3 numeroinen numero on mahdollinen
-            $sum = $terve[7] + $terve[8] + $terve[9];
-            if($sum < 2 || $sum > 27){
-                $hei = $hei + 1;
-            }
-            else{
-                $hei = $hei + 0;
-            }
+        if(strlen($terve) == 11){   
+            
+            //tarkistaa onko 1,2,3,4,5,6,8,9,10  merkit numeroita ja onko 7,11 merkit merkkejä
+            
+            if(is_numeric($terve[0]) && is_numeric($terve[1]) && is_numeric($terve[2])
+            && is_numeric($terve[3]) && is_numeric($terve[4]) && is_numeric($terve[5])
+            && is_string($terve[6]) && is_string($terve[10])
+            && is_numeric($terve[7]) && is_numeric($terve[8]) && is_numeric($terve[9])) {
 
-            //katsoo jos 9 numeroisen luvun jakojäännöksen ja katsoo jos tarkistus kirjain on oikea
-            $yht = $terve[0].$terve[1].$terve[2].$terve[3].$terve[4].$terve[5].$terve[7].$terve[8].$terve[9];
-            $ref = $yht % 31;
+                    $hei = $hei + 0;
+                //katsoo jos päivämäärä voi olla oikea
+                if($terve[0].$terve[1] > 31 || $terve[0].$terve[1] < 1){
+                    $hei = $hei + 1;
+                }
+                else{
+                    $hei = $hei + 0;
+                }
+                //katsoo jos kuukausi voi olla oikea
+                if($terve[2].$terve[3] > 12 || $terve[2].$terve[3] < 1){
+                    $hei = $hei + 1;
+                }
+                else{
+                    $hei = $hei + 0;
+                }
+                //katsoo jos vuosi voi olla oikea
+                if($terve[4].$terve[5] > 99 || $terve[4].$terve[5] < 0){
+                    $hei = $hei + 1;
+                }
+                else{
+                    $hei = $hei + 0;
+                }
+                //katsoo jos 7 inputin kirjai on A , - tai +
+                if (str_contains($terve[6], '-')||str_contains($terve[6], 'A')||str_contains($terve[6], '+')){
+                    $hei = $hei + 0;
+                }
+                else{
+                    $hei = $hei +1;
+                    
+                }
+                //katsoo jos 3 numeroinen numero on mahdollinen
+                $sum = $terve[7].$terve[8].$terve[9];
+                if($sum < 001 || $sum > 999){
+                    $hei = $hei + 1;
+                }
+                else{
+                    $hei = $hei + 0;
+                }
 
-            if($terve[10] == $lista[$ref]){
-                $hei = $hei + 0;
-            }
-            else{
+                //katsoo jos 9 numeroisen luvun jakojäännöksen ja katsoo jos tarkistus kirjain on oikea
+                $yht = $terve[0].$terve[1].$terve[2].$terve[3].$terve[4].$terve[5].$terve[7].$terve[8].$terve[9];
+                $ref = $yht % 31;
+
+                if($terve[10] == $lista[$ref]){
+                    $hei = $hei + 0;
+                }
+                else{
+                    $hei = $hei + 1;
+                }
+
+            }else{
                 $hei = $hei + 1;
-            }
-
+            }   
+            
 
 
         }
