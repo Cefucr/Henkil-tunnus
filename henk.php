070dbf8a7a2b945d757ab9374d37f481384bba9b
Väinö Jämsä <105347805+Cefucr@users.noreply.github.com>
2022-09-21 09:58:12 +0300

    Update henk.php

--- henk.php
+++ henk.php
@@ -46,80 +46,60 @@
             
             if(is_numeric($terve[0]) && is_numeric($terve[1]) && is_numeric($terve[2])
             && is_numeric($terve[3]) && is_numeric($terve[4]) && is_numeric($terve[5])
-            && is_string($terve[6]) && is_string($terve[10])
-            && is_numeric($terve[7]) && is_numeric($terve[8]) && is_numeric($terve[9])) {
+            && str_contains($terve[6], '-')||str_contains($terve[6], 'A')||str_contains($terve[6], '+')
+            && ctype_alpha($terve[10])
+            && is_numeric($terve[7]) && is_numeric($terve[8]) && is_numeric($terve[9])){
 
-                    $hei = $hei + 0;
                 //katsoo jos päivämäärä voi olla oikea
                 if($terve[0].$terve[1] > 31 || $terve[0].$terve[1] < 1){
-                    $hei = $hei + 1;
-                }
-                else{
-                    $hei = $hei + 0;
+                    $hei++;
                 }
                 //katsoo jos kuukausi voi olla oikea
                 if($terve[2].$terve[3] > 12 || $terve[2].$terve[3] < 1){
-                    $hei = $hei + 1;
-                }
-                else{
-                    $hei = $hei + 0;
+                    $hei++;
                 }
                 //katsoo jos vuosi voi olla oikea
                 if($terve[4].$terve[5] > 99 || $terve[4].$terve[5] < 0){
-                    $hei = $hei + 1;
+                    $hei++;
                 }
-                else{
-                    $hei = $hei + 0;
-                }
+                
                 //katsoo jos 7 inputin kirjai on A , - tai +
-                if (str_contains($terve[6], '-')||str_contains($terve[6], 'A')||str_contains($terve[6], '+')){
-                    $hei = $hei + 0;
-                }
-                else{
-                    $hei = $hei +1;
-                    
+                if (!(str_contains($terve[6], '-')||str_contains($terve[6], 'A')||str_contains($terve[6], '+'))){
+                    $hei++;
                 }
                 //katsoo jos 3 numeroinen numero on mahdollinen
                 $sum = $terve[7].$terve[8].$terve[9];
                 if($sum < 001 || $sum > 999){
-                    $hei = $hei + 1;
+                    $hei++;
                 }
-                else{
-                    $hei = $hei + 0;
-                }
-
+                
                 //katsoo jos 9 numeroisen luvun jakojäännöksen ja katsoo jos tarkistus kirjain on oikea
                 $yht = $terve[0].$terve[1].$terve[2].$terve[3].$terve[4].$terve[5].$terve[7].$terve[8].$terve[9];
                 $ref = $yht % 31;
 
-                if($terve[10] == $lista[$ref]){
-                    $hei = $hei + 0;
-                }
-                else{
-                    $hei = $hei + 1;
+                if($terve[10] != $lista[$ref]){
+                    $hei++;
                 }
 
             }else{
-                $hei = $hei + 1;
+                $hei++;
             }   
-            
-
 
         }
         else{
-            $hei = $hei + 1;
+            $hei++;
         }
 
         //kertoo jos id on oikea tai väärennetty
         if($hei >= 1){
             echo "Väärennetty ID id: ",$terve;
         }
-        else if($hei < 1){
+        else{
             echo "ID on tosi";
 
         }
        
     }
     
-    
+//AarniLove    
 ?>
